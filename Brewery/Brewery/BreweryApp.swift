670507c5408865aa93b972d3556d7169922876f5
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-04-06 14:20:14 -0300

    First commit

---
+++ Brewery/Brewery/BreweryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreweryApp.swift
+//  Brewery
+//
+//  Created by Gustavo Araujo Santos on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct BreweryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brewery/Brewery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Brewery
+//
+//  Created by Gustavo Araujo Santos on 06/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
