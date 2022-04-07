4856f0c6b5221f6495e308e8453018326e6e6ed8
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-04-07 12:09:39 -0300

    refactor: Created components folder structure

---
+++ Brewery/Brewery/Presentation/Components/CustomTextField.swift
@@ -0,0 +1,43 @@
+//
+//  CustomTextField.swift
+//  Brewery
+//
+//  Created by Gustavo Araujo Santos on 07/04/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct CustomTextField: View {
+    @Binding var text: String
+    let placeholder: String
+    let systemImageName: String
+
+    var body: some View {
+        ZStack {
+            HStack {
+                Image(systemName: systemImageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                TextField(placeholder, text: $text)
+            }
+        }
+    }
+}
+
+struct DefaultCustomTextFieldLayout: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(10)
+            .background(.white)
+            .cornerRadius(5)
+            .padding(.horizontal)
+    }
+}
+
+extension View {
+    func defaultLayoutTextField() -> some View {
+        modifier(DefaultCustomTextFieldLayout())
+    }
+}
--- Brewery/Brewery/Presentation/Home/HomeView.swift
+++ Brewery/Brewery/Presentation/Home/HomeView.swift
@@ -7,40 +7,6 @@
 
 import SwiftUI
 
-struct CustomTextField: View {
-    @Binding var text: String
-    let placeholder: String
-    let systemImageName: String
-
-    var body: some View {
-        ZStack {
-            HStack {
-                Image(systemName: systemImageName)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 20, height: 20)
-                TextField(placeholder, text: $text)
-            }
-        }
-    }
-}
-
-struct DefaultCustomTextFieldLayout: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .padding(10)
-            .background(.white)
-            .cornerRadius(5)
-            .padding(.horizontal)
-    }
-}
-
-extension View {
-    func defaultLayoutTextField() -> some View {
-        modifier(DefaultCustomTextFieldLayout())
-    }
-}
-
 struct HomeView: View {
     @State private var searchText = ""
     var body: some View {
