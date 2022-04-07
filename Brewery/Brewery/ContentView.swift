1a792fdd9351e3fe5a5543e319a106ab3d76b5e2
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-04-07 10:59:04 -0300

    feat: Home screen layout start

--- Brewery/Brewery/ContentView.swift
+++ Brewery/Brewery/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText = ""
     var body: some View {
-        Text("test xcode commit")
-            .padding()
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 254/255, green: 204/255, blue: 0/255), location: 0.3),
+                .init(color: Color(red: 1, green: 1, blue: 1), location: 0.3)
+            ], center: .top, startRadius: 100, endRadius: 400)
+                .ignoresSafeArea()
+
+            VStack {
+                Text("Bem vindo,\nEncontre as melhores cervejarias")
+                    .font(.headline)
+
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
+                    TextField("Nome da Cidade", text: $searchText)
+                }
+                    .padding(10)
+                    .background(.white)
+                    .cornerRadius(5)
+                    .padding(.horizontal)
+
+                Spacer()
+            }
+        }
     }
 }
 
