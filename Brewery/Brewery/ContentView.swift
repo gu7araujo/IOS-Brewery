3f9f3091e5bb27b1fb9cd7df7c43b04a8af87fe8
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-04-07 12:00:48 -0300

    refactor: Separating textField in a component

--- Brewery/Brewery/ContentView.swift
+++ Brewery/Brewery/ContentView.swift
@@ -7,6 +7,40 @@
 
 import SwiftUI
 
+struct CustomTextField: View {
+    @Binding var text: String
+    let placeholder: String
+    let systemImageName: String
+
+    var body: some View {
+        ZStack {
+            HStack {
+                Image(systemName: systemImageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                TextField(placeholder, text: $text)
+            }
+        }
+    }
+}
+
+struct DefaultCustomTextFieldLayout: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(10)
+            .background(.white)
+            .cornerRadius(5)
+            .padding(.horizontal)
+    }
+}
+
+extension View {
+    func defaultLayoutTextField() -> some View {
+        modifier(DefaultCustomTextFieldLayout())
+    }
+}
+
 struct ContentView: View {
     @State private var searchText = ""
     var body: some View {
@@ -21,17 +55,9 @@ struct ContentView: View {
                 Text("Bem vindo,\nEncontre as melhores cervejarias")
                     .font(.headline)
 
-                HStack {
-                    Image(systemName: "magnifyingglass")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
-                    TextField("Nome da Cidade", text: $searchText)
-                }
-                    .padding(10)
-                    .background(.white)
-                    .cornerRadius(5)
-                    .padding(.horizontal)
+                CustomTextField(text: $searchText, placeholder: "Nome da cidade", systemImageName: "magnifyingglass")
+                    .defaultLayoutTextField()
+
 
                 Spacer()
             }
