ef33ca9c96905a0ff2ba3175465e6811220b89de
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-04-08 15:45:44 -0300

    refactor: Removing spaces from home and adding message

--- Brewery/Brewery/Presentation/Home/HomeView.swift
+++ Brewery/Brewery/Presentation/Home/HomeView.swift
@@ -14,20 +14,33 @@ struct HomeView: View {
             RadialGradient(stops: [
                 .init(color: Color(red: 254/255, green: 204/255, blue: 0/255), location: 0.3),
                 .init(color: Color(red: 1, green: 1, blue: 1), location: 0.3)
-            ], center: .top, startRadius: 100, endRadius: 500)
+            ], center: .top, startRadius: 200, endRadius: 300)
                 .ignoresSafeArea()
 
             VStack {
-                Text("Bem vindo,\nEncontre as melhores cervejarias")
-                    .font(.headline)
-                    .foregroundColor(.black)
+                VStack(alignment: .leading) {
+                    Text("Bem vindo,\nEncontre as melhores cervejarias")
+                        .font(.headline)
 
-                CustomTextField(text: $searchText, placeholder: "Nome da cidade", systemImageName: "magnifyingglass")
-                    .defaultLayoutTextField()
+                    CustomTextField(text: $searchText, placeholder: "Buscar local", systemImageName: "magnifyingglass")
+                        .defaultLayoutTextField()
+                }
 
+                Spacer()
+
+                VStack {
+                    Text("Nenhum termo digitado")
+                        .fontWeight(.semibold)
+                        .font(.title)
+                        .padding()
+
+                    Text("Por favor, verifique sua pesquisa e tente novamente para obter resultados.")
+                        .font(.subheadline)
+                }
 
                 Spacer()
             }
+            .padding()
         }
     }
 }
