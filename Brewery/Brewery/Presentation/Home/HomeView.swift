1f1905f43938aebd54aefb87ecc61df3b8fd1307
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-04-09 10:10:50 -0300

    feat: Adding initial session url to home

--- Brewery/Brewery/Presentation/Home/HomeView.swift
+++ Brewery/Brewery/Presentation/Home/HomeView.swift
@@ -26,6 +26,7 @@ private struct Messages: View {
 
 struct HomeView: View {
     @State private var searchText = ""
+
     var body: some View {
         ZStack {
             RadialGradient(stops: [
@@ -113,11 +114,96 @@ struct HomeView: View {
             }
             .padding()
         }
+        .onAppear {
+            Task {
+                await sendApiCall()
+            }
+        }
     }
+
+    func sendApiCall() async {
+        guard let result = await ApiManager.shared.sendRequest(parameters: [:]) else {
+            return
+        }
+
+        switch result {
+        case .success(let data):
+            print(data)
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+enum RequestType: String {
+    case getRequest = "GET"
+}
+
+struct Brewery: Decodable {
+    let id, name, breweryType: String
+    let street: String?
+    let city, state, postalCode, country: String
+    let longitude, latitude, phone: String?
+    let websiteURL: String?
+    let updatedAt, createdAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case breweryType = "brewery_type"
+        case street, city, state
+        case postalCode = "postal_code"
+        case country, longitude, latitude, phone
+        case websiteURL = "website_url"
+        case updatedAt = "updated_at"
+        case createdAt = "created_at"
+    }
+}
+
+class ApiManager {
+    private let baseURL = "https://api.openbrewerydb.org/breweries"
+
+    static var shared = ApiManager()
+
+    private var request: URLRequest?
+
+    private init () {}
+
+    func sendRequest(parameters: [String: Any]) async -> Result<[Brewery], Error>? {
+        do {
+            guard let urlRequest = createGetRequestWithURLComponents(parameters: parameters) else {
+                return nil
+            }
+
+            let (data, _) = try await URLSession.shared.data(for: urlRequest)
+
+            let parsedData = try JSONDecoder().decode([Brewery].self, from: data)
+
+            return .success(parsedData)
+        } catch {
+            return .failure(error)
+        }
+    }
+
+    private func createGetRequestWithURLComponents(parameters: [String: Any]) -> URLRequest? {
+        var components = URLComponents(string: baseURL)!
+
+        components.queryItems = parameters.map({ (key: String, value: Any) in
+            URLQueryItem(name: key, value: "\(value)")
+        })
+
+        components.percentEncodedQuery = components.percentEncodedQuery?.replacingOccurrences(of: "+", with: "%2B")
+
+        request = URLRequest(url: components.url!)
+
+        request?.httpMethod = RequestType.getRequest.rawValue
+
+        return request
     }
 }
